8a96736ae881cf0f473e7262aea95767f15fb9ed
MandyTrl <thorelmandy@gmail.com>
2024-04-08 15:49:15 +0200

    add navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import ChitChatsScreen from "./screens/ChitChatsScreen"
+import HomeScreen from "./screens/HomeScreen"
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Hello !</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Welcome" component={HomeScreen} />
+				<Tab.Screen name="ChitChats" component={ChitChatsScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-})
---
+++ screens/ChitChatsScreen.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, ScrollView, View, Button } from "react-native"
+
+export const ChitChatsScreen = () => {
+	return (
+		<ScrollView contentContainerStyle={styles.container}>
+			<Text style={styles.title}>Tous les chats</Text>
+			<StatusBar style="auto" />
+		</ScrollView>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#ffe5d9",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 32,
+		fontWeight: "700",
+		backgroundColor: "#ffff",
+		padding: 20,
+		borderRadius: 20,
+	},
+})
+
+export default ChitChatsScreen
