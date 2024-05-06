801dfedca17e3f6d2003b6a7cb7a4fe3b1a817af
MandyTrl <thorelmandy@gmail.com>
2024-05-06 17:59:17 +0200

    add some style to chat channel

--- frontend/app/discussion/[slug]/page.tsx
+++ frontend/app/discussion/[slug]/page.tsx
@@ -5,20 +5,25 @@ import { Navbar } from "@/components/Navigation/Navbar"
 import { TextArea } from "@/components/UI/TextArea"
 import { useWebSocket } from "@/utils/hooks/useWebSocket"
 
+export type MsgType = {
+	username: string
+	msg: string
+}
+
 export default function Discussion() {
 	const pathname = usePathname()
 	const { socket, username } = useWebSocket()
 	const channel = pathname.split("/")[2].replace(/%20/g, " ")
 	const socketChannel = channel === "board game" ? "boardgame" : channel
-	const [msg, setMsg] = useState<string>("")
+	const [messages, setMessages] = useState<MsgType[]>([])
 
 	useEffect(() => {
 		if (socket) {
 			socket.once(`msgFromChannel${socketChannel}`, ({ user, message }) => {
-				setMsg(`${user} : ${msg}`)
+				setMessages([...messages, { username: user, msg: message }])
 			})
 		}
-	}, [msg, socketChannel])
+	}, [messages, socketChannel])
 
 	return (
 		<main className="w-full h-screen">
@@ -33,9 +38,15 @@ export default function Discussion() {
 			<div
 				id="discussion__container"
 				className="w-full h-4/5 rounded-sm bg-amber-100/70 p-2 mt-2 shadow-sm overflow-y-scroll">
-				{msg !== "" && (
-					<p className="bg-white/80 py-1 px-2 rounded-md">{msg}</p>
-				)}
+				{messages.length !== 0 &&
+					messages.map((el: MsgType, idx: number) => {
+						return (
+							<div key={idx} className="mx-2 py-2">
+								<p className="font-semibold  mb-1">{el.username}</p>
+								<p className="bg-white/80 py-1 px-2 rounded-md">{el.msg}</p>
+							</div>
+						)
+					})}
 			</div>
 
 			<TextArea
